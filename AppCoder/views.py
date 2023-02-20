68035ac48e97972196598c3de48b82d891f284c6
Mateo Zabala <mateozabala84@gmail.com>
2023-02-20 01:06:06 -0300

    la ultima

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from AppCoder.models import *
+from AppCoder.models import Curso
 # Create your views here.
 
 def inicio(request):
-   return HttpResponse("vista inicio")   
-
+   return render(request, 'AppCoder/inicio.html')
+   #return HttpResponse("vista inicio")   
 
 def curso (request):
-   return HttpResponse("vista cursos")
+   return render(request, 'AppCoder/cursos.html')
+   #return HttpResponse("vista cursos")
 
 def profesores (request):
-   return HttpResponse("vista profesores")
+   return render(request, 'AppCoder/profesores.html')
+   #return HttpResponse("vista profesores")
 
 def entregables (request):
-   return HttpResponse("vista entregables")
+   return render(request, 'AppCoder/entregables.html')
+   #return HttpResponse("vista entregables")
 
 def estudiantes (request):
-   return HttpResponse("vista estudiantes")
+   return render(request, 'AppCoder/estudiantes.html')
+   #return HttpResponse("vista estudiantes")
